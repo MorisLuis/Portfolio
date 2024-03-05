c92e78b60dda27f0b885c51fc9235b01e6c49fe5
Morado <moradoluisenrique@gmail.com>
2024-03-04 18:50:11 -0600

    S / Menu finished and contact links

--- components/navigation/Menu.tsx
+++ components/navigation/Menu.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styles from "../../styles/Navigation.module.scss";
 import Link from 'next/link';
+import Image from 'next/image';
+
 export const Menu = () => {
-    return  (
+    return (
         <div className={styles.menu}>
             <div className={styles.menuContent}>
                 <Link href={"/"} className={styles.link}>
@@ -11,9 +13,39 @@ export const Menu = () => {
                 <Link href={"/contact"} className={styles.link}>
                     Contact
                 </Link>
-                <Link href={"/contact"} className={styles.link}>
-                    Projects
-                </Link>
+                <div className={styles.projects}>
+                    <p className={styles.link}>Projects</p>
+                    <div className={styles.images}>
+                        <Link href={"/project/sicre_ctm"}>
+                            <Image
+                                src={'/image3.png'}
+                                alt='Project'
+                                width={200}
+                                height={200}
+                            />
+                            <p>Sicre CTM</p>
+                        </Link>
+                        <Link href={"/project/olei_online"}>
+                            <Image
+                                src={'/image3.png'}
+                                alt='Project'
+                                width={200}
+                                height={200}
+                            />
+                            <p>Olei Online</p>
+                        </Link>
+                        <Link href={"/project/olei_app"}>
+                            <Image
+                                src={'/image3.png'}
+                                alt='Project'
+                                width={200}
+                                height={200}
+                            />
+                            <p>Olei App</p>
+                        </Link>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
